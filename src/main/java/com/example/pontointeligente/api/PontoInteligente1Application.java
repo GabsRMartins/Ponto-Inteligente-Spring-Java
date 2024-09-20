4a0772967fe622782aa22692ee3137ed130283cc
Gabs <gabsmartins262@gmail.com>
2024-09-20 14:48:12 -0300

    Commit inicial dos arquivos

---
+++ src/main/java/com/example/pontointeligente/api/PontoInteligente1Application.java
@@ -0,0 +1,13 @@
+package com.example.pontointeligente.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoInteligente1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoInteligente1Application.class, args);
+	}
+
+}
